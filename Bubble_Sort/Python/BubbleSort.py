b380a58b2eb50d5f6e9f2ea225101fa10d06502c
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-11-16 22:17:11 +0100

    [Bubble sort] naive implementation

--- Bubble_Sort/Python/BubbleSort.py
+++ Bubble_Sort/Python/BubbleSort.py
@@ -1,3 +1,27 @@
 
-def bubbleSort(array):
+def bubble_sort(array):
+    while not array_is_ordered(array):
+        bubble_first_value(array)
+
     return array
+
+
+def array_is_ordered(array):
+    for index, value in enumerate(array):
+        if need_switch_with_next_value(index, array):
+            return False
+    return True
+
+
+def bubble_first_value(array):
+    for index, value in enumerate(array):
+        if need_switch_with_next_value(index, array):
+            switch_current_and_next_value(index, array)
+
+
+def need_switch_with_next_value(index, array):
+    return (index+1 < len(array)) and (array[index] > array[index+1])
+
+
+def switch_current_and_next_value(index, array):
+    (array[index], array[index+1]) = (array[index+1], array[index])
--- Bubble_Sort/Python/BubbleSortTestCase.py
+++ Bubble_Sort/Python/BubbleSortTestCase.py
@@ -2,11 +2,17 @@ import unittest
 
 from BubbleSort import *
 
+
 class BubbleSortTestCase(unittest.TestCase):
     def test_should_sort_array_of_positive_numbers(self):
         array = [6, 3, 0, 5, 1]
         array_expected = [0, 1, 3, 5, 6]
-        self.assertEqual(array_expected, bubbleSort(array))
+        self.assertEqual(array_expected, bubble_sort(array))
+
+    def test_should_sort_array_of_positive__and_negative_numbers(self):
+        array = [-4, 6, 3, 0, -33, 5, 1]
+        array_expected = [-33, -4, 0, 1, 3, 5, 6]
+        self.assertEqual(array_expected, bubble_sort(array))
 
 
 if __name__ == '__main__':
