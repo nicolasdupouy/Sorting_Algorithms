04ecc2b938c9314d13c28bbf8a7eeafe2833e705
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-11-21 18:46:34 +0100

    [Bubble sort] Improve time analysis

--- Bubble_Sort/Python/BubbleSortTestCase.py
+++ Bubble_Sort/Python/BubbleSortTestCase.py
@@ -18,21 +18,8 @@ class BubbleSortTestCase(unittest.TestCase):
         self.assertEqual(array_expected, bubble_sort_not_so_naive_implementation(array))
 
     def test_bubble_sort_timeit(self):
-        self.time_function_with_one_million_calls(bubble_sort, [-4, 6, 3, 0, -33, 5, 1])
-        self.time_function_with_one_million_calls(bubble_sort_not_so_naive_implementation, [-4, 6, 3, 0, -33, 5, 1])
-
-    @staticmethod
-    def time_function_with_one_million_calls(f, array):
-        import timeit
-        start = timeit.default_timer()
-
-        for t in range(1000000):
-            f(array)
-
-        stop = timeit.default_timer()
-        execution_time_in_seconds = stop - start
-
-        print(f"Program Executed in [function {f.__name__}] "+str(execution_time_in_seconds))
+        bubble_sort_one_million_calls([-4, 6, 3, 0, -33, 5, 1])
+        bubble_sort_not_so_naive_implementation_one_million_calls([-4, 6, 3, 0, -33, 5, 1])
 
 
 if __name__ == '__main__':
